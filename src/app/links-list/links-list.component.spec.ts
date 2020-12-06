12b45200326e53bacd518612d192a70f9a24a3c5
Kate Sky <katesky@MacBook-Pro.local>
2020-12-05 21:17:21 -0600

    update to 11

--- src/app/links-list/links-list.component.spec.ts
+++ src/app/links-list/links-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LinksListComponent } from './links-list.component';
 
@@ -6,7 +6,7 @@ describe('LinksListComponent', () => {
   let component: LinksListComponent;
   let fixture: ComponentFixture<LinksListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LinksListComponent ]
     })
